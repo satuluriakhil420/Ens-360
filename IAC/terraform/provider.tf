225fb1e7bcf47dce12b81ea266a02c5b504103b9
satuluriakhil420 <94843341+satuluriakhil420@users.noreply.github.com>
2024-07-19 20:58:43 +0530

    Update provider.tf

--- IAC/terraform/provider.tf
+++ IAC/terraform/provider.tf
@@ -2,12 +2,12 @@ terraform {
   backend "s3" {
     bucket         = "sentrics-terraform"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-1"
     dynamodb_table = "terraform"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region           = "us-east-1"
+  region           = "us-west-1"
 }
